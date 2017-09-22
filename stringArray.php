030e86ff661ca6ad1dacd7dc6c947bd609b9ee29
Tanuka Nayak <tn76@afsconnect1.njit.edu>
2017-09-22 18:30:27 -0400

    added echothis function

--- stringArray.php
+++ stringArray.php
@@ -1,7 +1,9 @@
 <?php
 	$obj=new main();			//Defining main class for String functions
 	$str = chr(046);
-	$obj->chr($str);			
+	$obj->chr($str);
+	$text = "learning PHP in IS601";
+	$obj->echothis($text);
 	class main {
 		public function __construct() { 
 			echo "<CENTER><h1>String functions</h1></CENTER>";
@@ -10,7 +12,13 @@
 			echo '<h2>1. chr function</h2>';
 			echo("Web Design $str Development"); 
 			echo '<hr>';
-		}			
+		}
+		public function echothis($text) {
+		        echo '<h2>2. echo function</h2>';
+		        echo 'It is fun when you are ' .$text;
+			echo '<hr>';
+		}
+														    
 	}					
 ?>
 
